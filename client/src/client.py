21e187fd4057db2b1c4126b56e8d461332cff5ca
mmsamiei <mohmahsamiei@gmail.com>
2017-05-21 22:10:16 +0430

    some

--- client/src/client.py
+++ client/src/client.py
@@ -2,14 +2,11 @@ import socket
 import time
 
 host = socket.gethostbyname(socket.gethostname()) 
-port = 1995
+port = 1997
 buffer_size = 1 * 1024 
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((host,port))
-data = clientSocket.recv(buffer_size).decode()
-print("recived data is : {}".format(data))
-#clientSocket.send("Hello".encode())
-time.sleep(1)
+clientSocket.connect((host,port)) 
+print(clientSocket.recv(buffer_size).decode())
 clientSocket.send("LIST samiei hastam".encode())
 clientSocket.close()
--- proxy_server/src/new_proxy_server.py
+++ proxy_server/src/new_proxy_server.py
@@ -4,8 +4,8 @@ import os,socket,threading
 import re
 
 my_ip = socket.gethostbyname(socket.gethostname())
-ftp_control_port = 1995
-max_connection = 5 
+ftp_control_port = 1997
+max_connection = 5
 buffer_size = 1 * 1024
 
 ceit_host = socket.gethostbyname('ceit.aut.ac.ir')
@@ -26,7 +26,7 @@ class FTPserver (threading.Thread):
 					conn, addr = self.control_socket.accept()
 					client_thread = FTPclientThread(conn, addr)
 					client_thread.start()
-				except KeyboardInterrupt:
+				except KeyboardInterrupt:#TODO kill al threads
 					print("[*] (Proxy)Server Starts To Finishing")
 					control_socket.close()
 					sys.exit(1)
@@ -42,6 +42,8 @@ class FTPclientThread(threading.Thread):
 		threading.Thread.__init__(self)
 		self.http_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.http_socket.connect((ceit_host,80))
+		self.dataAddr = addr[0]
+		self.dataport = ftp_control_port +1 
 
 
 	def run(self):
@@ -58,32 +60,45 @@ class FTPclientThread(threading.Thread):
 				function = getattr(self,cmd[0:4].strip().upper())
 				function(cmd)
 
-	def RMD(self,cmd):
+	def RMD(self, cmd):
 		print("1")
 	
-	def DELE(self,cmd):
+	def DELE(self, cmd):
 		print("1")		
 	
-	def RETR(self,cmd):
+	def RETR(self, cmd):
 		print("1")
 	
+	def PORT(self, cmd):
+		l = cmd[5:].split(',')
+		self.dataAddr = '.'.join(l[:4])
+		self.dataport = (int(l[4])<<8)+int(l[5])
+		self.conn.send('200 GET PORT. \r\n'.encode())
+
 	def LIST(self,cmd):
-		print("YES")
+		print("LIST")
 		URL_HEAD = "HEAD /~94131090/CN1_Project_Files/ HTTP/1.1\r\nHost: {}\r\n\r\n".format(ceit_host)
 		URL_GET = "GET /~94131090/CN1_Project_Files/ HTTP/1.1\r\nHost: {}\r\n\r\n".format(ceit_host)
 		self.http_socket.send(URL_GET.encode())
-		result = self.http_socket.recv(buffer_size).decode()
+		result = self.reliable_recv(buffer_size).decode() ## must correct size
+		print("!!!!")
 		http_status = result[9:12]
 		if http_status == "200":
 			content_len = self.get_content_len(result)
+			self.http_socket.recv(2*content_len - buffer_size) ## TODO mush correct the size of 2*contentlength
 			self.http_socket.send(URL_GET.encode())
-			result = self.http_socket.recv(content_len+1).decode()
-			regex = r'<(.*)>'
-			matchObj = re.search(regex, result, re.M|re.I)
-			print(matchObj.group(0))
-			print(matchObj.group(1))
-			print(matchObj.group(2))
-			
+			#result = self.http_socket.recv(2*content_len).decode()	
+			result = self.reliable_recv(content_len).decode()
+			regex = re.compile("<a.*>(.*)(\..*)</a>")
+			rose = regex.findall(result)
+			sended_msg = ""
+			for ros in rose:
+				sended_msg  = sended_msg + str(rose[0]) + "." + str(rose[1]) + "\r\n"
+			self.start_datasocket()	
+			self.datasocket.send(sended_msg.encode())
+			self.stop_datasocket()
+			self.control_socket.send('226 List Was Sent Everything Is Ok \r\n'.encode())
+
 		else :
 			print("BAD")	
 	
@@ -99,6 +114,22 @@ class FTPclientThread(threading.Thread):
 			b = b + 1
 		i = int(result[a:b])
 		return i
+
+	def reliable_recv(self, size):
+		chunks = bytes()
+		bytes_recd = 0 
+		while bytes_recd < size:
+			chunk = self.http_socket.recv(min(size - bytes_recd, buffer_size))
+			if chunk == '':
+				raise RuntimeError("socket connection broken")
+			chunks = chunks + chunk
+			bytes_recd = bytes_recd + len(chunk)
+		return chunks
+
+	def start_datasocket(self):
+		self.datasocket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.datasocket.connect((self.dataAddr, self.dataport))
+
 
 if __name__ == '__main__':
 	ftp = FTPserver()
